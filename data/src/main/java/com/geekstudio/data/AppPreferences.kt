cd456cc23162b0c6fd33585ae742d3270a553bd1
BaeWooRam <41356481+BaeWooRam@users.noreply.github.com>
2021-08-23 17:50:28 +0900

    [Update] 메소드 이름 수정

--- data/src/main/java/com/geekstudio/data/AppPreferences.kt
+++ data/src/main/java/com/geekstudio/data/AppPreferences.kt
@@ -12,9 +12,9 @@ class AppPreferences(private val context: Context) {
      * NotificationTime 저장
      * @author 배우람
      */
-    fun setNotificationTime(time:String){
+    fun setNotificationWorkTime(time:String){
         val editor = context.getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE).edit()
-        editor.putString(SHARED_PREFERENCE_REST_NOTIFICATION_TIME, time)
+        editor.putString(SHARED_PREFERENCE_WORK_NOTIFICATION_TIME, time)
         editor.commit()
     }
 
@@ -22,18 +22,18 @@ class AppPreferences(private val context: Context) {
      * NotificationTime 가져오기
      * @author 배우람
      */
-    fun getNotificationTime():String?{
+    fun getNotificationWorkTime():String?{
         val editor = context.getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
-        return editor.getString(SHARED_PREFERENCE_REST_NOTIFICATION_TIME, null)
+        return editor.getString(SHARED_PREFERENCE_WORK_NOTIFICATION_TIME, null)
     }
 
     /**
      * NotificationTime 지우기
      * @author 배우람
      */
-    fun clearNotificationTime():Boolean{
+    fun clearNotificationWorkTime():Boolean{
         val editor = context.getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE).edit()
-        editor.remove(SHARED_PREFERENCE_REST_NOTIFICATION_TIME)
+        editor.remove(SHARED_PREFERENCE_WORK_NOTIFICATION_TIME)
         return editor.commit()
     }
 
@@ -68,7 +68,7 @@ class AppPreferences(private val context: Context) {
 
     companion object{
         const val SHARED_PREFERENCE_NAME = "Pomodoro"
-        const val SHARED_PREFERENCE_NOTIFICATION_TIME = "NotificationTime"
+        const val SHARED_PREFERENCE_WORK_NOTIFICATION_TIME = "NotificationWorkTime"
         const val SHARED_PREFERENCE_REST_NOTIFICATION_TIME = "NotificationRestTime"
     }
 }
