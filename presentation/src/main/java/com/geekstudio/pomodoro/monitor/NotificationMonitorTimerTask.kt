c2c5d6702f5606a4f93e48ac2d270f4f8bbabe9e
BaeWooRam <41356481+BaeWooRam@users.noreply.github.com>
2021-08-23 17:50:39 +0900

    Update NotificationMonitorTimerTask.kt

--- presentation/src/main/java/com/geekstudio/pomodoro/monitor/NotificationMonitorTimerTask.kt
+++ presentation/src/main/java/com/geekstudio/pomodoro/monitor/NotificationMonitorTimerTask.kt
@@ -1,13 +1,17 @@
 package com.geekstudio.pomodoro.monitor
 
 import android.util.Log
-import com.geekstudio.data.notification.BaseNotification
 import com.geekstudio.data.notification.RestNotification
 import com.geekstudio.data.notification.WorkNotification
 import com.geekstudio.data.repository.local.NotificationTimeLocalDataSourceImp
+import com.geekstudio.data.kakao.SendKakao
+import com.geekstudio.data.send.SendKakaoBuilder
 import com.geekstudio.entity.NotificationState
-import com.geekstudio.entity.TimeUnit
 import com.geekstudio.pomodoro.Config
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Exception
 import java.util.*
 
 /**
@@ -18,8 +22,9 @@ class NotificationMonitorTimerTask(
     private val notificationTimeLocalDataSourceImp: NotificationTimeLocalDataSourceImp,
     private val restNotification: RestNotification,
     private val workNotification: WorkNotification,
-) : TimerTask() {
+) : TimerTask(), SendKakao.SmsListener {
     private val debugTag: String = javaClass.simpleName
+    private val scope = CoroutineScope(Dispatchers.Default)
     private var state: NotificationState = NotificationState.Work
     private var time: Int = 0
 
@@ -27,23 +32,60 @@ class NotificationMonitorTimerTask(
         Log.d(javaClass.simpleName, "NotificationMonitorTimerTask start time = $time")
         time += Config.CONNECTION_MONITOR_TIME
 
+        val workMessageSender = getWorkMessageSender()
+        val restMessageSender = getRestMessageSender()
+
         when (state) {
             NotificationState.Work -> {
-                if (notificationTimeLocalDataSourceImp.getNotificationRestTime().toSecond() == time){
+                if (notificationTimeLocalDataSourceImp.getNotificationWorkTime().toSecond() != time)
+                    return
+
+                scope.launch {
                     Log.d(javaClass.simpleName, "WorkNotification send()")
                     workNotification.send()
+                    workMessageSender?.send()
                     state = NotificationState.Rest
                     time = 0
                 }
             }
             NotificationState.Rest -> {
-                if (notificationTimeLocalDataSourceImp.getNotificationRestTime().toSecond() == time){
+                if (notificationTimeLocalDataSourceImp.getNotificationRestTime().toSecond() != time)
+                    return
+
+                scope.launch {
                     Log.d(javaClass.simpleName, "RestNotification send()")
                     restNotification.send()
+                    restMessageSender?.send()
                     state = NotificationState.Work
                     time = 0
                 }
             }
         }
+    }
+
+
+    private fun getWorkMessageSender(): SendKakaoBuilder.Sender? {
+        return SendKakaoBuilder()
+            .target("01092055472")
+            .message("일하세요")
+            .listener(this@NotificationMonitorTimerTask)
+            .build()
+    }
+
+
+    private fun getRestMessageSender(): SendKakaoBuilder.Sender? {
+        return SendKakaoBuilder()
+            .target("01092055472")
+            .message("휴식하세요")
+            .listener(this@NotificationMonitorTimerTask)
+            .build()
+    }
+
+    override fun onSuccess() {
+        Log.d(javaClass.simpleName, "SendSms is onSuccess!")
+    }
+
+    override fun onFailure(e: Exception) {
+        Log.d(javaClass.simpleName, "SendSms is Failure : ${e.message}")
     }
 }
